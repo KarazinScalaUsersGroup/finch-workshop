d70edba8d59d7d216e88bbe7162af31a818fea67
Igor Wolkov <igor.wolkov@kredito.de>
2017-04-26 23:07:28 +0300

    01 Hello World

--- src/main/scala/karazinscalausersgroup/workshop/`finch workshop`.scala
+++ src/main/scala/karazinscalausersgroup/workshop/`finch workshop`.scala
@@ -1,10 +1,31 @@
-package karazinscalausersgroup.workshop
+package  karazinscalausersgroup.workshop
+
+import com.twitter.finagle.Http
+import com.twitter.util.Await
+import io.finch._
 
 /**
-  * @author Igor Wolkov
+  * A simple 'Hello World' Finch application
+  *
+  * Use the following sbt command to run the application.
+  *
+  * {{{
+  *   $ sbt sbt 'runMain karazinscalausersgroup.workshop.finch$u0020workshop'
+  * }}}
+  *
+  * Use the following HTTPie/curl commands to test endpoints.
+  *
+  * {{{
+  *   $ http GET :8080/hello
+  * }}}
   */
 object `finch workshop` extends App {
 
 
+  val api: Endpoint[String] = get("hello") { Ok("Hello, World!") }
+
+  val service = Http.server.serve(":8080", api.toServiceAs[Text.Plain])
+
+  Await.ready(service)
 
 }
