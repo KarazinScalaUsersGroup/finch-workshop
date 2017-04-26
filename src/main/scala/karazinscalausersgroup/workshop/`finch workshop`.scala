f822a37bfe7d8946775e9c73bb3029803e7d8ede
Igor Wolkov <igor.wolkov@kredito.de>
2017-04-26 23:11:30 +0300

    02 Echo Server

--- src/main/scala/karazinscalausersgroup/workshop/`finch workshop`.scala
+++ src/main/scala/karazinscalausersgroup/workshop/`finch workshop`.scala
@@ -1,10 +1,31 @@
 package karazinscalausersgroup.workshop
 
+import com.twitter.finagle.Http
+import com.twitter.util.Await
+import io.finch._
+
 /**
-  * @author Igor Wolkov
+  * A simple Finch Echo Server application
+  *
+  * Use the following sbt command to run the application.
+  *
+  * {{{
+  *   $ sbt sbt 'runMain karazinscalausersgroup.workshop.finch$u0020workshop'
+  * }}}
+  *
+  * Use the following HTTPie/curl commands to test endpoints.
+  *
+  * {{{
+  *   $ http GET :8080/hello/world
+  * }}}
   */
 object `finch workshop` extends App {
 
+  val api: Endpoint[String] = get("hello" :: string) { name: String => Ok(s"Hello, $name!") }
 
+  val service = Http.server.serve(":8080", api.toServiceAs[Text.Plain])
+
+  Await.ready(service)
 
 }
+
