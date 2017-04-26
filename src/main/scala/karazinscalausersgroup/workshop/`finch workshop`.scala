f44e80b9880676312451630d287088352ab30378
Igor Wolkov <igor.wolkov@kredito.de>
2017-04-27 00:10:20 +0300

    09 CRUD: Update with patch

--- src/main/scala/karazinscalausersgroup/workshop/`finch workshop`.scala
+++ src/main/scala/karazinscalausersgroup/workshop/`finch workshop`.scala
@@ -14,7 +14,9 @@ import com.twitter.server.TwitterServer
 import com.twitter.finagle.stats.Counter
 
 /**
-  * A simple Finch application
+  * A simple CRUD Finch application
+  *
+  * Phase 7. Update with patch
   *
   * Use the following sbt command to run the application.
   *
@@ -22,9 +24,53 @@ import com.twitter.finagle.stats.Counter
   *   $ sbt sbt 'runMain karazinscalausersgroup.workshop.finch$u0020workshop'
   * }}}
   *
+  * Use the following HTTPie/curl commands to test endpoints.
+  *
+  * {{{
+  *   $ http GET :8080/get/ticket/uuid
+  *
+  *   $ http POST :8080/add/ticket uuid=e5c06c7e-a2b5-45f8-a5e7-90de7ede1f76 reporter="Descartes" assignee="Newton" description="Create cool Finch workshop"
+  *
+  *   $ http POST :8080/update/ticket uuid=e5c06c7e-a2b5-45f8-a5e7-90de7ede1f76 reporter="Descartes" assignee="Leibniz" description="Create cool Finch workshop"
+  * }}}
   */
 object `finch workshop` extends App {
 
+  val `get ticket`: Endpoint[Ticket] =
+    get("get" :: "ticket" :: uuid) { uuid: UUID =>
+      storage get uuid match {
+        case Some(ticket) => Ok(ticket)
+        case None         => NoContent
+      }
+    }
 
+  val `add ticket`: Endpoint[Ticket] =
+    post("add" :: "ticket" :: jsonBody[Ticket]) { ticket: Ticket =>
+      storage put ticket
+
+      Ok(ticket)
+    }
+
+  val patched: Endpoint[Ticket => Ticket] = jsonBody[Ticket => Ticket]
+
+  val `update ticket`: Endpoint[Ticket] =
+    post("update" :: "ticket" :: uuid :: patched) { (uuid: UUID, pt: Ticket => Ticket) =>
+
+      storage get uuid match {
+        case Some(oldTicket) =>
+          val `updated ticket` = pt(oldTicket)
+          storage put `updated ticket`
+          Ok(`updated ticket`)
+
+        case None =>
+          NoContent
+      }
+    }
+
+  val api = `get ticket` :+: `add ticket` :+: `update ticket`
+
+  val service = Http.server.serve(":8080", api.toServiceAs[Application.Json])
+
+  Await.ready(service)
 
 }
